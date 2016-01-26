fb2074e5865a9bd430cbdd797a412f823b8e041b
Alexey Savkin <alexi_el@inbox.ru>
2016-01-26 15:05:01 +0300

    add about equipment

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
  */
 
 //= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/magnific-popup/dist/jquery.magnific-popup.min.js
 
 
 
--- src/js/partials/common.js
+++ src/js/partials/common.js
@@ -1,5 +1,23 @@
-$(document).ready(function () {
-
-	
-
+$(document).ready(function() {
+	$('.gallery_wrap').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
 });
